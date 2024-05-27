263e3d3ef2d2e74e46291f6138b7a4a16a8ccf6b
Julia Enriquetto de Brito <juliaenriquettodebrito@gmail.com>
2024-05-27 12:09:44 -0300

    feat: finish fetchUser function

--- SwiftUI-Auth/SwiftUI-Auth/Core/Authentication/AuthViewModel.swift
+++ SwiftUI-Auth/SwiftUI-Auth/Core/Authentication/AuthViewModel.swift
@@ -16,6 +16,10 @@ class AuthViewModel: ObservableObject {
     
     init() {
         self.userSession = Auth.auth().currentUser
+        
+        Task {
+            await fetchUser()
+        }
     }
     
     func signIn(withEmail email: String, password: String) async throws {
@@ -30,6 +34,7 @@ class AuthViewModel: ObservableObject {
             let user = User(id: result.user.uid, fullname: fullname, email: email)
            let encodedUser = try Firestore.Encoder().encode(user)
             try await Firestore.firestore().collection("users").document(user.id).setData(encodedUser)
+            await fetchUser()
         } catch {
             print("DEBUG: Failed to create user with error \(error.localizedDescription)")
         }
@@ -45,5 +50,7 @@ class AuthViewModel: ObservableObject {
     
     func fetchUser() async {
         guard let uid = Auth.auth().currentUser?.uid else { return }
+        guard let snapshot = try? await Firestore.firestore().collection("users").document(uid).getDocument() else { return }
+        self.currentUser = try? snapshot.data(as: User.self)
     }
 }
--- SwiftUI-Auth/SwiftUI-Auth/Core/Profile/ProfileView.swift
+++ SwiftUI-Auth/SwiftUI-Auth/Core/Profile/ProfileView.swift
@@ -8,54 +8,58 @@
 import SwiftUI
 
 struct ProfileView: View {
+    @EnvironmentObject var viewModel: AuthViewModel
+    
     var body: some View {
-        List {
-            Section {
-                HStack{
-                    Text(User.MOCK_USER.initials)
-                        .font(.title)
-                        .fontWeight(.semibold)
-                        .foregroundColor(.white)
-                        .frame(width: 72, height: 72)
-                        .background(Color(.systemGray3))
-                        .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
-                    
-                    VStack(alignment: .leading, spacing: 4) {
-                        Text(User.MOCK_USER.fullname)
-                            .font(.subheadline)
+        if let user = viewModel.currentUser {
+            List {
+                Section {
+                    HStack{
+                        Text(user.initials)
+                            .font(.title)
                             .fontWeight(.semibold)
-                            .padding(.top, 4)
+                            .foregroundColor(.white)
+                            .frame(width: 72, height: 72)
+                            .background(Color(.systemGray3))
+                            .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
                         
-                        Text(User.MOCK_USER.email)
-                            .font(.footnote)
+                        VStack(alignment: .leading, spacing: 4) {
+                            Text(user.fullname)
+                                .font(.subheadline)
+                                .fontWeight(.semibold)
+                                .padding(.top, 4)
+                            
+                            Text(user.email)
+                                .font(.footnote)
+                                .foregroundColor(.gray)
+                        }
+                    }
+                }
+                
+                Section("General") {
+                    HStack {
+                        SettingsRowView(imageName: "genr", title: "Version", tintColor: Color(.systemGray))
+                        
+                        Spacer()
+                        
+                        Text("1.0.0")
+                            .font(.subheadline)
                             .foregroundColor(.gray)
                     }
                 }
-            }
-            
-            Section("General") {
-                HStack {
-                    SettingsRowView(imageName: "genr", title: "Version", tintColor: Color(.systemGray))
-                    
-                    Spacer()
-                    
-                    Text("1.0.0")
-                        .font(.subheadline)
-                        .foregroundColor(.gray)
-                }
-            }
-            
-            Section("Account") {
-                Button {
-                    print("Sign out...")
-                } label: {
-                    SettingsRowView(imageName: "arrow.left.circle.fill", title: "Sign Out", tintColor: .red)
-                }
                 
-                Button {
-                    print("Delete account...")
-                } label: {
-                    SettingsRowView(imageName: "xmark.circle.fill", title: "Delete Account", tintColor: .red)
+                Section("Account") {
+                    Button {
+                        print("Sign out...")
+                    } label: {
+                        SettingsRowView(imageName: "arrow.left.circle.fill", title: "Sign Out", tintColor: .red)
+                    }
+                    
+                    Button {
+                        print("Delete account...")
+                    } label: {
+                        SettingsRowView(imageName: "xmark.circle.fill", title: "Delete Account", tintColor: .red)
+                    }
                 }
             }
         }
